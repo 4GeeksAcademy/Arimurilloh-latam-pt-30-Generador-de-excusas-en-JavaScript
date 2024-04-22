5c1a25ce9d0299b6cce1231ca44090923e8b956c
Arimurilloh <161553052+Arimurilloh@users.noreply.github.com>
2024-04-22 03:26:46 +0000

    Se crea el codigo excuse generator en app.js, en HTML creo el div y en css se ajusta el codigo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generadorExcusa() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const whoRandom = Math.floor(Math.random() * who.length);
+  const actionRandom = Math.floor(Math.random() * who.length);
+  const whatRandom = Math.floor(Math.random() * who.length);
+  const whenRandom = Math.floor(Math.random() * who.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let excuse = `${who[whoRandom]} ${action[actionRandom]} ${what[whatRandom]} ${when[whenRandom]}`;
+
+  return excuse;
+}
+
+function showExcuse() {
+  let excuseId = document.getElementById("excuse");
+  // let excuseRandom = generadorExcusa();
+  excuseId.innerHTML = generadorExcusa();
+}
+
+window.onload = showExcuse();
