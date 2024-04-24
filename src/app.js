ec7f7453dc1cbed89df8331a51dd0a26629ac2c1
Arimurilloh <161553052+Arimurilloh@users.noreply.github.com>
2024-04-24 02:23:50 +0000

    Se agrega la variable let con valor excuse para la generación de excusa aleatoria

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+import "./style.css";
+
 function generadorExcusa() {
+  // Excusas
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
@@ -7,23 +10,30 @@ function generadorExcusa() {
     "when I was sleeping",
     "while I was exercising",
     "during my lunch",
-    "while I was praying"
+    "while I was praying",
   ];
-
+  // Aleatoriedad para arrays
   const whoRandom = Math.floor(Math.random() * who.length);
-  const actionRandom = Math.floor(Math.random() * who.length);
-  const whatRandom = Math.floor(Math.random() * who.length);
-  const whenRandom = Math.floor(Math.random() * who.length);
+  const actionRandom = Math.floor(Math.random() * action.length);
+  const whatRandom = Math.floor(Math.random() * what.length);
+  const whenRandom = Math.floor(Math.random() * when.length);
 
-  let excuse = `${who[whoRandom]} ${action[actionRandom]} ${what[whatRandom]} ${when[whenRandom]}`;
+  //Concatenar
+  let excusa = `${who[whoRandom]} ${action[actionRandom]} ${what[whatRandom]} ${when[whenRandom]}.`;
 
-  return excuse;
+  return excusa;
 }
 
+// Función nueva para mostrar excusa en HTML
 function showExcuse() {
+  // Conseguir el id de "excuse"
   let excuseId = document.getElementById("excuse");
-  // let excuseRandom = generadorExcusa();
-  excuseId.innerHTML = generadorExcusa();
+
+  // Generación de excusa aleatoria de función generadorExcusa
+  let excuse = generadorExcusa();
+
+  // Mostrar en HTML
+  excuseId.innerHTML = excuse;
 }
 
 window.onload = showExcuse();
